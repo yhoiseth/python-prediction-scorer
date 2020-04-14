c3f6986818d7a4a8ebdf4c880f13abf3eb3e7186
Yngve Høiseth <yngve@hoiseth.net>
2020-04-14 23:13:24 +0200

    Cleanup

--- tests/test_brier.py
+++ tests/test_brier.py
@@ -1,23 +1,13 @@
 import decimal
 
 from predictionscorer import predictions
-from predictionscorer import (
-    calculators,
-)  # Brier scoring is arguably the most common way of scoring predictions.
+from predictionscorer import calculators
 
 
 class TestBrier:
-    def test_readme(self):
-        george = predictions.Prediction(
-            probabilities=[60, 40]
-        )  # George put Clinton at 60 % and Trump at 40 %.
-        kramer = predictions.Prediction(
-            probabilities=[35, 65]
-        )  # Kramer put Clinton at 35 % and Trump at 65 %.
-
-        calculator = calculators.Brier(
-            true_alternative_index=1
-        )  # Alternative 0 is Hillary Clinton. Alternative 1 is Donald Trump.
-
+    def test_readme_example(self):
+        george = predictions.Prediction(probabilities=[60, 40])
+        kramer = predictions.Prediction(probabilities=[35, 65])
+        calculator = calculators.Brier(true_alternative_index=1)
         assert calculator.calculate(george) == decimal.Decimal("0.72")
         assert calculator.calculate(kramer) == decimal.Decimal("0.245")
