a5b0cacb0ce7101b7bd764792d659073e1987e29
Yngve Høiseth <yngve@hoiseth.net>
2020-04-14 16:18:10 +0200

    Configure project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    author="Yngve Hoiseth",
+    author_email="yngve@hoiseth.net",
+    description="Python library to score predictions",
+    license="MIT",
+    name="predictionscorer",
+    packages="predictionscorer",
+    url="https://github.com/yhoiseth/python-prediction-scorer",
+    version="0.1.0",
+    zip_safe=False,
+)
