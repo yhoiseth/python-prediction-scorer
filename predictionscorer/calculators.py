5a8b19f74aeb74b94fc89de14d40725240d0b4d5
Yngve Høiseth <yngve@hoiseth.net>
2020-04-14 22:47:13 +0200

    Make more readable

--- predictionscorer/calculators.py
+++ predictionscorer/calculators.py
@@ -12,9 +12,9 @@ class Brier:
     def calculate(self, prediction: predictions.Prediction) -> decimal.Decimal:
         score = decimal.Decimal(0.00)
         for index, probability in enumerate(prediction.probabilities):
+            first_term = decimal.Decimal(probability) / decimal.Decimal(100)
             second_term = decimal.Decimal(
                 1.00 if index == self.true_alternative_index else 0.00
             )
-            first_term = decimal.Decimal(probability) / decimal.Decimal(100)
             score = score + (first_term - second_term) ** 2
         return score
