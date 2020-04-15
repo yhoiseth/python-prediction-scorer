8acfe567bad450216f2790ebfa4bd99c1e50c16d
Yngve Høiseth <yngve@hoiseth.net>
2020-04-15 12:32:02 +0200

    Assert probabilities sum

--- predictionscorer/predictions.py
+++ predictionscorer/predictions.py
@@ -7,4 +7,5 @@ class Prediction:
 
     def __init__(self, probabilities: typing.List[decimal.Decimal]) -> None:
         assert len(probabilities) >= 2, "A prediction needs at least two probabilities."
+        assert sum(probabilities) == 100, "Probabilities need to sum to 100."
         self.probabilities = probabilities
