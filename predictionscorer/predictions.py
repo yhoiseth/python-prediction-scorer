ede6adc26b2c8dfe447b9be4c02c7d2e99d93315
Yngve Høiseth <yngve@hoiseth.net>
2020-04-15 12:12:52 +0200

    Assert probabilities count

--- predictionscorer/predictions.py
+++ predictionscorer/predictions.py
@@ -6,4 +6,5 @@ class Prediction:
     probabilities: typing.List[decimal.Decimal]
 
     def __init__(self, probabilities: typing.List[decimal.Decimal]) -> None:
+        assert len(probabilities) >= 2, "A prediction needs at least two probabilities."
         self.probabilities = probabilities
