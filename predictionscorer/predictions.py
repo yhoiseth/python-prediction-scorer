eb82066017c1c84ed73936349bd35d52723c8d25
Yngve Høiseth <yngve@hoiseth.net>
2020-04-15 11:46:47 +0200

    Switch to decimals

--- predictionscorer/predictions.py
+++ predictionscorer/predictions.py
@@ -1,8 +1,9 @@
+import decimal
 import typing
 
 
 class Prediction:
-    probabilities: typing.List[int]
+    probabilities: typing.List[decimal.Decimal]
 
-    def __init__(self, probabilities: typing.List[int]) -> None:
+    def __init__(self, probabilities: typing.List[decimal.Decimal]) -> None:
         self.probabilities = probabilities
--- tests/test_brier.py
+++ tests/test_brier.py
@@ -5,13 +5,23 @@ from predictionscorer import calculators, predictions
 
 class TestBrier:
     def test_readme_example(self):
-        george = predictions.Prediction(probabilities=[60, 40])
-        kramer = predictions.Prediction(probabilities=[35, 65])
-        calculator = calculators.Brier(true_alternative_index=1)
+        george = predictions.Prediction(
+            probabilities=[decimal.Decimal(60), decimal.Decimal(40),]
+        )
+        kramer = predictions.Prediction(
+            probabilities=[decimal.Decimal(35), decimal.Decimal(65),]
+        )
+        calculator = calculators.Brier(true_alternative_index=1,)
         assert calculator.calculate(george) == decimal.Decimal("0.72")
         assert calculator.calculate(kramer) == decimal.Decimal("0.245")
 
     def test_readme_example_more_than_two_alternatives(self):
-        prediction = predictions.Prediction(probabilities=[55, 35, 10,])
+        prediction = predictions.Prediction(
+            probabilities=[
+                decimal.Decimal(55),
+                decimal.Decimal(35),
+                decimal.Decimal(10),
+            ]
+        )
         brier = calculators.Brier(true_alternative_index=1,)
         assert brier.calculate(prediction) == decimal.Decimal("0.735")
