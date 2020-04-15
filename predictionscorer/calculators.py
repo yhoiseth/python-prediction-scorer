bf87abf89140415e7903be6454addcf398ab37f2
Yngve Høiseth <yngve@hoiseth.net>
2020-04-15 11:53:57 +0200

    Decimal inputs (#12)
    * Document decimal inputs
    
    * Switch to decimals
    
    * Log changes

--- predictionscorer/calculators.py
+++ predictionscorer/calculators.py
@@ -10,11 +10,11 @@ class Brier:
         self.true_alternative_index = true_alternative_index
 
     def calculate(self, prediction: predictions.Prediction) -> decimal.Decimal:
-        score = decimal.Decimal(0.00)
+        score = decimal.Decimal("0.00")
         for index, probability in enumerate(prediction.probabilities):
-            first_term = decimal.Decimal(probability) / decimal.Decimal(100)
+            first_term = probability / decimal.Decimal(100)
             second_term = decimal.Decimal(
-                1.00 if index == self.true_alternative_index else 0.00
+                "1.00" if index == self.true_alternative_index else "0.00"
             )
             score = score + (first_term - second_term) ** 2
         return score
