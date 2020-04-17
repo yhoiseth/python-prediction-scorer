946174244f63325e1529e1ae8bd5afc2428baf40
Yngve Høiseth <yngve@hoiseth.net>
2020-04-17 16:16:17 +0200

    Draw chart

--- plot.py
+++ plot.py
@@ -1,4 +1,26 @@
+import decimal
+import typing
+
 import matplotlib.pyplot as plt
 
-plt.plot([1, 2, 3, 4], [1, 4, 2, 3])
+from predictionscorer import calculators, predictions
+
+calculator = calculators.Brier(true_alternative_index=0)
+
+all_predictions: typing.List[predictions.Prediction] = []
+
+for index in range(0, 101):
+    all_predictions.append(
+        predictions.Prediction([decimal.Decimal(index), decimal.Decimal(100 - index),])
+    )
+
+x_axis_data: typing.List[decimal.Decimal] = []
+y_axis_data: typing.List[decimal.Decimal] = []
+
+for prediction in all_predictions:
+    x_axis_data.append(prediction.probabilities[0])
+    y_axis_data.append(calculator.calculate(prediction))
+
+
+plt.plot(x_axis_data, y_axis_data)
 plt.show()
