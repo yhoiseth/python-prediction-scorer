5552affa1aea66ccbb80389516335a1cdd628d00
Yngve Høiseth <yngve@hoiseth.net>
2020-06-10 16:47:59 +0200

    Test additional probabilities

--- tests/test_binary.py
+++ tests/test_binary.py
@@ -3,7 +3,10 @@ from predictionscorer.binary import Prediction
 
 class TestBrier:
     def test_0_percent(self):
-        probability = 0
-        prediction = Prediction(probability)
-        assert prediction.probability == 0
-        assert prediction.brier == 2
+        assert Prediction(0).brier == 2
+
+    def test_50_percent(self):
+        assert Prediction(50).brier == 0.5
+
+    def test_100_percent(self):
+        assert Prediction(100).brier == 0
