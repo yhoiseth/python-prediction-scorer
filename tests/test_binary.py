7f0496b29645929531c96232e376cc76ceb844df
Yngve Høiseth <yngve@hoiseth.net>
2020-06-18 15:45:19 +0200

    Add test

--- tests/test_binary.py
+++ tests/test_binary.py
@@ -62,6 +62,17 @@ class TestQuadratic:
 
 
 class TestRelativeQuadratic:
+    def test_20_80_100(self):
+        collection = Collection((Prediction(20), Prediction(80), Prediction(100)))
+        assert collection.median_quadratic == Decimal("0.92")
+        predictions = collection.predictions
+        _20 = predictions[0]
+        _80 = predictions[1]
+        _100 = predictions[2]
+        assert _20.relative_quadratic == Decimal("-1.20")
+        assert _80.relative_quadratic == 0
+        assert _100.relative_quadratic == Decimal("0.08")
+
     def test_40_65(self):
         collection = Collection((Prediction(40), Prediction(65)))
         assert collection.median_quadratic == Decimal("0.5175")
