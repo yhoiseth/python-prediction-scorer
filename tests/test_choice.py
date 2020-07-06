f522713d9c1db13045dd75114d8009a8cb0e17c8
Yngve Hoiseth <yngve@hoiseth.net>
2020-07-06 18:43:39 +0200

    Refactor

--- tests/test_choice.py
+++ tests/test_choice.py
@@ -34,15 +34,14 @@ class TestBrier:
 
 class TestRelativeBrier:
     def test_20_80_100(self):
-        collection = Collection((Prediction(20), Prediction(80), Prediction(100)))
+        bad = Prediction(20)
+        good = Prediction(80)
+        perfect = Prediction(100)
+        collection = Collection((bad, good, perfect))
         assert collection.median_brier == Decimal("0.08")
-        predictions = collection.predictions
-        _20 = predictions[0]
-        _80 = predictions[1]
-        _100 = predictions[2]
-        assert _20.relative_brier == Decimal("1.20")
-        assert _80.relative_brier == 0
-        assert _100.relative_brier == Decimal("-0.08")
+        assert bad.relative_brier == Decimal("1.20")
+        assert good.relative_brier == 0
+        assert perfect.relative_brier == Decimal("-0.08")
 
     def test_40_65(self):
         collection = Collection((Prediction(40), Prediction(65)))
