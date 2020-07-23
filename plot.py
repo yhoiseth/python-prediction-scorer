df0aa4d621bd39607a1caefa1a188dc8df9e794b
Yngve Høiseth <yngve@hoiseth.net>
2020-07-23 09:36:34 +0200

    Hide title, use correct X-axis

--- plot.py
+++ plot.py
@@ -11,20 +11,17 @@ from predictionscorer.rules import (
 )
 
 probabilities: List[float] = []
-x_axis_data: List[int] = []
 for index in range(1, 100):
     probabilities.append((index / 100))
-    x_axis_data.append(index)
 
 y_axis_data: List[Decimal] = []
 
 for probability in probabilities:
     y_axis_data.append(brier_score(probability))
 
-plt.plot(x_axis_data, y_axis_data)
+plt.plot(probabilities, y_axis_data)
 plt.xlabel("Probability assigned to correct answer")
 plt.ylabel("Brier score")
-plt.title("Brier score for probabilities 1 % - 99 %")
 plt.draw()
 plt.savefig("docs/charts/brier.svg")
 
@@ -35,10 +32,9 @@ y_axis_data = []
 for probability in probabilities:
     y_axis_data.append(logarithmic_score(probability))
 
-plt.plot(x_axis_data, y_axis_data)
+plt.plot(probabilities, y_axis_data)
 plt.xlabel("Probability assigned to correct answer")
 plt.ylabel("Logarithmic score")
-plt.title("Logarithmic score for probabilities 1 % - 99 %")
 plt.draw()
 plt.savefig("docs/charts/logarithmic.svg")
 
@@ -49,10 +45,9 @@ y_axis_data = []
 for probability in probabilities:
     y_axis_data.append(practical_score(probability))
 
-plt.plot(x_axis_data, y_axis_data)
+plt.plot(probabilities, y_axis_data)
 plt.xlabel("Probability assigned to correct answer")
 plt.ylabel("Practical score")
-plt.title("Practical score for probabilities 1 % - 99 %")
 plt.draw()
 plt.savefig("docs/charts/practical.svg")
 
@@ -63,9 +58,8 @@ y_axis_data = []
 for probability in probabilities:
     y_axis_data.append(quadratic_score(probability))
 
-plt.plot(x_axis_data, y_axis_data)
+plt.plot(probabilities, y_axis_data)
 plt.xlabel("Probability assigned to correct answer")
 plt.ylabel("Quadratic score")
-plt.title("Quadratic score for probabilities 1 % - 99 %")
 plt.draw()
 plt.savefig("docs/charts/quadratic.svg")
